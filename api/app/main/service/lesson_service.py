6e6dfb6f4ba93eb104b1e5d6c12799fc538af164
connor <connormullett@gmail.com>
2020-04-30 19:00:42 -0400

    added lesson service functions

--- api/app/main/service/lesson_service.py
+++ api/app/main/service/lesson_service.py
@@ -27,6 +27,40 @@ def save_new_lesson(data):
   }, 201
 
 
+def get_all_public_lessons():
+  return Lesson.query.filter_by(is_public=True)
+
+
+def get_all_public_lessons_by_module_id(module_id):
+  return Lesson.query.filter_by(module_id=module_id).filter_by(is_public=True).all()
+
+
+def get_all_lessons_by_module_id(module_id):
+  return Lesson.query.filter_by(module_id=module_id).all()
+
+
+def get_lesson_by_id(lesson_id):
+  return Lesson.query.filter_by(id=lesson_id).first()
+
+
+def update_lesson(lesson_id):
+  lesson = get_lesson_by_id(lesson_id)
+  for key, item in data.items():
+    setattr(lesson, key, item)
+  lesson.modified_at = datetime.datetime.utcnow()
+  db.session.commit()
+  return {
+    'status': 'updated module'
+  }, 200
+
+
+def delete_lesson_by_id(lesson_id):
+  lesson = get_lesson_by_id(lesson_id)
+  db.session.delete(lesson)
+  _save_changes()
+  return {'status': 'delete'}, 200
+
+
 def _save_changes(data):
   db.session.add(data)
   db.session.commit()
